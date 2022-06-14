c007af702bb06358a8edae2e39d8c9b44393d623
fansurirazak <45352643+fansurirazak@users.noreply.github.com>
2022-06-14 17:01:29 +0800

    Add project files.

---
+++ DHL_Demo/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DHL_Demo.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(DHL_Demo.Models.User model)
+        {
+            if(model.username == "admin" && model.password == "password")
+            {
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "User details are incorrect");
+                return View(model);
+            }
+        }
+    }
+}
---
+++ DHL_Demo/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DHL_Demo.Models
+{
+    public class User
+    {
+        [Display(Name = "Username")]
+        public string username { get; set; } = string.Empty;
+        [Display(Name = "Password")]
+        public string password { get; set; } = string.Empty;
+    }
+}
